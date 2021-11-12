6649b20239d5aafb9a36d593459dfe4d3162de3c
juhbm <jubmz@hotmail.com>
2021-11-12 07:38:52 -0300

    Produzindo Game Mania...

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+/* --- JAVASCRIPT --- */
+
+// alert('BOM DIA! \n Mensagem enviada pelo console')
+// console.log('Exemplo de comando alert');
+
+// let nome = prompt('Bem - Vindo(a) a Loja Game Mania. Qual o seu nome?')
+// alert( nome + ', é um prazer ter você em nosso site! \nNavegue a vontade e qualquer dúvida é só chamar.')
+
+
+
+
+
+/* --- JQUERY --- */
