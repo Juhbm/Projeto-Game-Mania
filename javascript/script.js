3b542055ef88fa0b417728887eb7e3c9fdd58f1e
juhbm <jubmz@hotmail.com>
2021-11-18 14:49:46 -0300

    Funções na index: JS add carrinho/ jQuery busca

--- javascript/script.js
+++ javascript/script.js
@@ -6,48 +6,64 @@
 // let nome = prompt('Bem - Vindo(a) a Loja Game Mania. Qual o seu nome?')
 // alert( nome + ', é um prazer ter você em nosso site! \nNavegue a vontade e qualquer dúvida é só chamar.')
 
+//-------------------------------------------------------------------------//
 
+    /*renderizar os prdoutos no cards criados na index.html*/
 function renderizarDestaques(){
     let destaque = document.getElementById("destaques")
-    
+
+     console.log("correndo");
+
     let produtos = [ /* lista dos produtos na seção destaque da index*/
-        {
+        {   
             imagem: "img/conjPS4.svg",
-            produto: "Conjunto PS4 Completo",
+            nome: "Conjunto PS4 Completo",
             precoantes: "De: R$ 7.549,90",
-            precodepois: "Por: R$ 5.999,90"
+            precodepois: "Por: R$ 5.999,90", /* alterei para número somente. porém é um float... o que deve estar interferindo na soma no carrinho*/
+            noCarrinho: 0,
+            tag: "um"
         },
         {
             imagem: "img/ps4Fifa22.svg",
-            produto: "Jogo FIFA22 PS4",
+            nome: "Jogo FIFA22 PS4",
             precoantes: "De: R$ 349,90",
-            precodepois: "Por: R$ 284,90"
+            precodepois: "Por: R$ 284,90",
+            noCarrinho: 0,
+            tag: "dois"
         },
         {
             imagem: "img/tecladoGamer.svg",
-            produto: "Teclado Gamer",
+            nome: "Teclado Gamer",
             precoantes: "De: R$ 298,90",
-            precodepois: "Por: R$ 199,90"
+            precodepois: "Por: R$ 199,90",
+            noCarrinho: 0,
+            tag: "tres"
         },
         {
             imagem: "img/cadeiraGamer.svg",
-            produto: "Cadeira Gamer",
+            nome: "Cadeira Gamer",
             precoantes: "De: R$ 1.229,90",
-            precodepois: "Por: R$ 899,90"
+            precodepois: "Por: R$ 899,90",
+            noCarrinho: 0,
+            tag: "quatro"
         },
         {
             imagem: "img/controlePS4.svg",
-            produto: "Par de Controle PS4",
+            nome: "Par de Controle PS4",
             precoantes: "De: R$ 749,90",
-            precodepois: "Por: R$ 659,90"
+            precodepois: "Por: R$ 659,90",
+            noCarrinho: 0,
+            tag: "cinco"
         },
         {
             imagem: "img/mouseGamer.svg",
-            produto: "Mouse Gamer",
+            nome: "Mouse Gamer",
             precoantes: "De: R$ 259,90",
-            precodepois: "Por: R$ 179,90"
-        },
-    ]
+            precodepois: "Por: R$ 179,90",
+            noCarrinho: 0,
+            tag: "seis"
+        }
+    ];
     
     let template = ""; /*variável para evitar bug de sobrescrever um sobre outro*/
 
@@ -58,13 +74,105 @@ function renderizarDestaques(){
                         <div class="icon">
                             <img src="${prod.imagem}" alt="Conjunto PS4">
                         </div>
-                        <h3>${prod.produto}</h3>
+                        <h3>${prod.nome}</h3>
                         <span>${prod.precoantes}}</span>
                         <h3>${prod.precodepois}</h3>
-                        <button>Comprar</button>
+                        <button class="add_carrinho">Comprar</button>
                     </div>`  /*template string*/
 
         destaque.innerHTML = template;
     }
+
+    /*ativando o botão de comprar para adicionar o item no carrinho de compras*/
+    /*add_carrinho é a classe que está no button comprar. Assim usamos querySelectorAll para puxar a classe de cada card de item.*/
+
+    let itens = document.querySelectorAll('.add_carrinho');
+
+    for (let i=0; i < itens.length; i++){
+
+        /*ao clicar adiciona produto no carrinho*/
+        itens[i].addEventListener('click', () => {
+            itensQuant(produtos[i]);
+        })
+
+        // console.log("meu loop"); testando se o for está funcionando.
+        // console.log("adicionado ao carrinho"); testando se ao clicar emite esse console.
+    }
+
+    function onloadItensQuant() { /*essa função fará que a qntdd de itens no carrinho não apague quando atualizar a página.*/
+        let prodQuant = localStorage.getItem('itensQuant');
+        
+        if (prodQuant){
+            document.getElementById("i_cart_total").textContent = prodQuant;
+        }
+    }
+
+    function itensQuant(produto) { /* função para adicionar/exibir itens no carrinho*/
+        
+        let prodQuant = localStorage.getItem('itensQuant');
+
+        prodQuant = parseInt(prodQuant); /* parseInt converte uma string para número*/
+       
+        if(prodQuant){
+            localStorage.setItem('itensQuant', prodQuant + 1); /*condição que fará adicionar + 1 quantidade do produto*/
+            document.getElementById("i_cart_total").textContent = prodQuant + 1;
+        } else{
+            localStorage.setItem('itensQuant', 1);
+            document.getElementById("i_cart_total").textContent = 1;
+        }
+
+        definindoItens(produto);
+    } 
+    
+    /* função que detalha a quantidade de cada produto que está sendo inserido no carrinho.*/
+    
+    function definindoItens(produto){
+        let itensCarrinho = localStorage.getItem('produtosNoCarrinho');
+        itensCarrinho = parseInt(itensCarrinho);
+        // itensCarrinho = JSON.parse(itensCarrinho);
+    
+        if(itensCarrinho != null){
+            
+            if(itensCarrinho[produto.tag] == undefined) {
+                itensCarrinho = {
+                    ...itensCarrinho,
+                    [produto.tag]: produto
+                }
+            }
+            itensCarrinho[produto.tag].noCarrinho += 1; /*problema que ainda não consegui resolver: considera que noCarrinho não é tipo string...*/
+        } else {
+            produto.noCarrinho = 1;
+            itensCarrinho = {
+                [produto.tag]: produto
+            }
+        }
+             
+        localStorage.setItem("produtosNocarrinho", JSON.stringify /*converte os valores de js para uma string json*/
+        (itensCarrinho));
+    }
+
+    onloadItensQuant();
 }
+
+    //localStorage.getItem armazena o valor no browser. Após atualizar tela, zera o carrinho. Exceto ao usar a função onloadItensQuant() criada acima.
+    // console.log(prodQuant)
+    // console.log (typeof prodQuant) exibir qual tipo está o caractere. Nesse caso: string. (antes do parseInt)
+    // console.log (typeof prodQuant) exibe a mudança string => número (após o parseInt)
+    //chamando document. (COM) getElementById ("i_cart_total") pq está como id no span do html. A bolinha de quantidades.
+    //console.log("O produto inserido é:", produto); testando abaixo da function itensQuant... qual o produto inserido.
+    // console.log("Dentro da função");
+    // console.log("Meu produto é: ", produto);
+
 /* --- JQUERY --- */ 
+
+/*função buscar com jQuery -- puxa dentro do input os produtos que tem na seção destaque*/
+
+        $(document).ready(function(){
+       $("#myInput").on("keyup", function(){
+        var value = $(this). val().toLowerCase();
+        $("#destaques .card").filter(function(){
+       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1); 
+
+        });
+    });
+});
